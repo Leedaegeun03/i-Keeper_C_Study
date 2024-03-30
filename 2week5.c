3515c55c5072a5c309d13ce72e2cdb1f3c2ed847
Leedaegeun03 <163279646+Leedaegeun03@users.noreply.github.com>
2024-03-30 21:17:30 +0900

    Create 2week5.c

---
+++ 2week5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main () {
+	int a, b, c;	
+	scanf("%d %d %d", &a, &b, &c);
+	if (a==b&&a==c)
+	printf("%d", a*1000 + 10000);
+	else if (a==c||a==b)
+	printf("%d", a*100 +1000);
+	else if (b==c)
+	printf("%d", b*100 + 1000);
+	else if (a>b&&a>c)
+	printf("%d", a*100);
+	else if (b>a&&b>c)
+	printf("%d", b*100);
+	else if (c>a&&c>b)
+	printf("%d", c*100);	
+	return 0;
+}

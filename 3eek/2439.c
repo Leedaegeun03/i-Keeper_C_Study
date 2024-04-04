1cf68229d9d3ff1e8aed80ed6c4afd3722a7923b
Leedaegeun03 <163279646+Leedaegeun03@users.noreply.github.com>
2024-04-05 03:57:44 +0900

    Create 2439.c

---
+++ 3eek/2439.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	int count;
+	scanf("%d", &count);
+	for (int a = 1; a <= count; a++) {
+		for (int b = count - a; b > 0&&b!= count; b--)
+			printf(" ");
+		for (int c = 0; c < a; c++)
+			printf("*");
+		
+		
+		printf("\n");
+		
+
+	}
+	
+	return 0;
+}
